147d1192b0b2c86fb515ae8be90b6aad4898389b
sayeed <expsayeedshorif@gmail.com>
2025-04-18 16:38:15 +0600

    added fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -26,5 +26,33 @@ function fibs(num){
     return arr;
 }
 
-let num = parseInt(prompt("Enter a valid number to get it's fibonacci sequences 😎 : "));
+let num = parseInt(prompt("Enter a valid number to get it's fibonacci sequences 😎 [simple code] : "));
 console.log(fibs(num));
+
+
+function fibsRec(num, arr = [0, 1]){
+    if (num < 0 || !Number.isInteger(num)) {
+        return 'Error ! enter a valid integer number 😑'
+    }
+    if (arr.length >= num) {
+        return arr;
+    }
+    return fibsRec(num, [...arr , arr[arr.length - 2] + arr[arr.length - 1]])
+}
+
+// user input
+
+let num2 = parseInt(prompt("Enter a valid number to get it's fibonacci sequences 😎 [recursive code] : "))
+
+console.log(fibsRec(num2));
+
+
+// example
+
+/*
+console.log(fibsRec(5));  //[ 0, 1, 1, 2, 3 ]
+console.log(fibsRec(-1));  // Error ! enter a valid number 😑
+console.log(fibsRec('hello')); // Error ! enter a valid number 😑
+console.log(fibsRec(4.3)); // Error ! enter a valid number 😑
+
+*/
